63862b1507c48706ec87e52c0d92f0fecd26fb54
Joanna Stachowicz <joannastachowicz19@gmail.com>
2020-07-06 00:23:54 +0200

    Toggle active class on element of thisProduct

--- src/js/script.js
+++ src/js/script.js
@@ -95,28 +95,28 @@
         event.preventDefault();
 
         /* toggle active class on element of thisProduct */
-
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
 
         /* find all active products */
-
+        
 
         /* START LOOP: for each active product */
-
+      
 
           /* START: if the active product isn't the element of thisProduct */
-
+         
 
             /* remove class active for the active product */
-
+           
 
             /* END: if the active product isn't the element of thisProduct */
-
+          
 
           /* END LOOP: for each active product */
-
+        
 
         /* END: click event listener to trigger */
-
+     
     }
   }
 
